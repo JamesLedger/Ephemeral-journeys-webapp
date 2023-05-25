4b440421283d46530b547fed5c8560a5b8c8949b
James Ledger <Jamesledger99@gmail.com>
2023-05-25 03:37:25 +0100

    Adding latest build

--- src/UI/GameContainer.tsx
+++ src/UI/GameContainer.tsx
@@ -1,30 +1,30 @@
-import styled from "styled-components";
-import { Unity, useUnityContext } from "react-unity-webgl";
-
+import styled from "styled-components"
+import { Unity, useUnityContext } from "react-unity-webgl"
 
 const GameWrapper = styled.div`
   background-color: red;
   width: 960px;
   height: 600px;
-`;
+`
 
 const UnityCanvas = styled(Unity)`
   width: 100%; /* Adjust the width as needed */
   height: 100%; /* Adjust the height as needed */
-`;
+`
 
 function GameContainer() {
-
   const { unityProvider } = useUnityContext({
     loaderUrl: "build/EphemeralJourneys.loader.js",
-    dataUrl: "build/webgl.data",
-    frameworkUrl: "build/build.framework.js",
-    codeUrl: "build/build.wasm",
-  });
+    dataUrl: "build/EphemeralJourneys.data",
+    frameworkUrl: "build/EphemeralJourneys.framework.js",
+    codeUrl: "build/EphemeralJourneys.wasm",
+  })
 
-  return <GameWrapper>
-    <UnityCanvas unityProvider={unityProvider} />
-  </GameWrapper>;
+  return (
+    <GameWrapper>
+      <UnityCanvas unityProvider={unityProvider} />
+    </GameWrapper>
+  )
 }
 
-export default GameContainer;
+export default GameContainer
